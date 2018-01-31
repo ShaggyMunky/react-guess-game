1059fc6c4221598709b2938b24aab6aca0080292
Aaron Guillen <aaronguillen21@gmail.com>
2018-01-31 14:40:58 -0800

    added user input updates

--- src/components/game.js
+++ src/components/game.js
@@ -4,9 +4,11 @@ class Game extends Component{
     constructor(props){
         super(props);
         this.state ={
-         randomNumber: 0
+            randomNumber: 0,
+            userNumber: ""
         };
-        this.resetGame = this.resetGame.bind(this)
+        this.resetGame = this.resetGame.bind(this);
+        this.makeGuess = this.makeGuess.bind(this);
     }
 
     componentDidMount(){
@@ -19,15 +21,35 @@ class Game extends Component{
             randomNumber: randNum
         });
     }
+    makeGuess(event){
+        event.preventDefault();
+        console.log("User Guess", this.state.userNumber)
+    }
     resetGame(){
         this.generateRandomNumber();
     }
     render(){
-        console.log("State: ", this.state);
+        console.log(this.state);
+        const btnStyle = {
+            margin: "10px 5px"
+        };
+        const{randomNumber, userNumber} = this.state;
         return(
             <div>
-                <p>Random Number: {this.state.randomNumber}</p>
-                <button onClick={this.resetGame} className="btn red darken-2">Reset</button>
+                <p>Random Number: {randomNumber}</p>
+                <div className="row">
+                    <form onSubmit={this.makeGuess}className="col s6 offset-s3">
+                        <div className="row">
+                            <div className="input-field">
+                                <input onChange={(event) => {this.setState({userNumber: event.target.value})}} value={userNumber} type="number" placeholder="Enter a number" className="center-align"/>
+                            </div>
+                        </div>
+                        <div className="row center-align">
+                            <button style={btnStyle} className="btn green darken-2">Guess</button>
+                            <button style={btnStyle} type="button" onClick={this.resetGame} className="btn red darken-2">Reset</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
